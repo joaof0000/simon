b3873c570d273fd6885e2f88461fe5bd20f1a1d2
joaof0000 <joaoifilipe@gmail.com>
2023-06-21 17:26:31 -0400

    5 round

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # simon
+
+Simon Game
+A simple, but captivating web version of the classic Simon game. This implementation uses HTML5, CSS3, and JavaScript to provide an engaging and interactive user experience. The game's goal is to follow an increasing sequence of flashing colors. The sequence gets longer each time the player completes the sequence correctly. The game continues until the player fails to follow the sequence.
+
+Image of Game
+
+Features
+Six different sections each having distinct colors and sounds.
+The game becomes progressively difficult as the sequence gets longer with each correct input.
+Mobile responsive design, the game can be played on devices of different screen sizes.
+Flashing animations to indicate active color in sequence.
+Score tracking to keep up with the player's level.
+How to Play
+Click the "Start Game" button.
+Watch and listen to the sequence presented.
+Repeat the sequence by clicking on the correct color sections in the correct order.
+If you complete the sequence correctly, a new step is added.
+If you fail to follow the sequence correctly, it's game over, and you can restart.
+The game continues until the player fails to match the sequence.
--- simon.js
+++ simon.js
@@ -1,77 +1,10 @@
-
-// // JavaScript Logic
-// Initialize game state variables:
-// - sequence: Array to store the sequence of panels
-// - userSequence: Array to store the user's input sequence
-// - level: Current level of the game
-// - gameStarted: Boolean flag to indicate if the game has started
-// - gameOver: Boolean flag to indicate if the game is over
-
-// Initialize sound effects for each panel:
-// - Assign a unique sound effect to each panel for later use.
-
-// // Event Handling
-// Add event listeners to panels:
-// - On panel click:
-//   - If gameStarted and !gameOver:
-//     - Add the ID or class of the clicked panel to the userSequence array.
-//     - Check user input against the expected sequence:
-//       - Iterate over each item in the userSequence array and compare it with the corresponding item in the sequence array.
-//       - If any item doesn't match, end the game and display the game over message.
-//     - If user input is correct:
-//       - Play the sound effect associated with the clicked panel.
-//       - If the userSequence length is equal to the sequence length:
-//         - Increment the level.
-//         - Generate a new random panel and add it to the sequence.
-//         - Reset the userSequence array.
-//         - Display the new sequence to the user using the displaySequence() function.
-//       - Continue the game.
-
-// // Game Flow
-// Function startGame():
-//   - Reset game state variables:
-//     - sequence = []
-//     - userSequence = []
-//     - level = 1
-//     - gameStarted = false
-//     - gameOver = false
-//   - Generate a random panel and add it to the sequence.
-//   - Display the initial sequence to the user using the displaySequence() function.
-//   - Set gameStarted to true.
-
-// // DOM Manipulation
-// Function displaySequence(sequence):
-//   - Iterate over each panel ID or class in the sequence array:
-//     - Highlight the panel visually for a short duration to indicate the next item in the sequence.
-//     - Play the corresponding sound effect associated with the panel.
-//     - Wait for a short delay before moving to the next panel in the sequence.
-
-// // Responsive Design
-// Apply responsive design techniques to ensure compatibility with different screen sizes and devices:
-// - Use CSS media queries to adjust the layout, sizing, and positioning of elements based on the screen size.
-// - Test the game on various devices and screen sizes to ensure a seamless experience.
-
-// // Testing and Debugging
-// Perform testing and debugging to ensure proper functioning of the game and address any issues encountered during development:
-// - Test various game scenarios, including correct and incorrect user input, reaching higher levels, and game over conditions.
-// - Use console.log() statements to log relevant information for debugging purposes.
-// - Implement error handling to gracefully handle unexpected scenarios.
-
-// // Other Considerations
-// Additional features and enhancements you can explore:
-// - Scoring mechanism: Keep track of the user's score based on the level they reached or the number of successful sequences completed.
-// - Levels of difficulty: Introduce different difficulty levels that increase the complexity of the game, such as shorter timeouts or faster sequence display.
-// - User interface improvements: Add visual feedback for correct and incorrect user input, provide hints or instructions to guide the user, and incorporate animations or transitions to enhance the overall user experience. -->
-//  -->
-
-
-
-
 let sequence = [];
 let humanSequence = [];
 let level = 0;
 let gameStarted = false;
 let gameOver = false;
+let turnTimeout;  
+
 
 // Define six different sounds
 const sounds = {
@@ -98,16 +31,30 @@ for (let i = 1; i <= 6; i++) {
 document.getElementById('start-button').addEventListener('click', startGame);
 
 function startGame() {
-    sequence = [];
-    humanSequence = [];
-    level = 0;
-    gameStarted = true;
-    gameOver = false;
+    sequence = [], humanSequence = [], level = 0;
+    gameStarted = true, gameOver = false;
+    renderWelcomeMessage();
     addToSequence();
-    playSequence();
     renderScore();
-    renderGameStatus('');
+    renderGameStatus('Game is starting in 5 seconds...');
+    setTimeout(() => {
+        renderGameStatus('');
+        playSequence();
+        resetCountdown();
+    }, 5000);
 }
+
+function renderWelcomeMessage() {
+    document.getElementById('welcome-message').textContent = 
+    "Welcome to Simon Game! You will see a sequence of buttons light up. Your task is to repeat the sequence by clicking the buttons in the same order. If you make a mistake, the game is over. The sequence gets one button longer each time you correctly repeat it. Good luck!";
+}
+
+
+// Event listener for flash speed change
+document.getElementById('flash-speed').addEventListener('change', (event) => {
+    document.documentElement.style.setProperty('--flash-speed', `${event.target.value}s`);
+});
+
 
 function addToSequence() {
     sequence.push(getRandomNumber());
@@ -119,37 +66,45 @@ const lightUp = (color) => {
     button.classList.add('active');
     setTimeout(() => {
         button.classList.remove('active');
-    }, 1500); // increased flash duration to 1.5 seconds
-    setTimeout(() => sounds[color].play(), 500); // Delay added to synchronize with button light
+    }, 1500);
+    setTimeout(() => sounds[color].play(), 500);
 };
 
 const playSequence = () => {
     sequence.forEach((color, index) => {
         setTimeout(() => {
             lightUp(color);
-        }, 2000 * (index + 1)); // increased delay between flashes to 2 seconds
+        }, 2000 * (index + 1));
     });
 };
 
-
 function humanTurn(color) {
+    clearTimeout(turnTimeout);
     if (humanSequence[humanSequence.length - 1] !== sequence[humanSequence.length - 1]) {
-        gameOver = true;
-        gameStarted = false;
-        renderGameStatus(`Game over! You reached level ${level}.`);
+        endGame();
     } else if (humanSequence.length === sequence.length && humanSequence.length !== 0) {
-        humanSequence = [];
-        level++;
-        if(level === 10){
-            renderGameStatus('Awesome work! You have reached level 10.');
-        }
+        humanSequence = [], level++;
         addToSequence();
         setTimeout(playSequence, 1000);
+        if(level === 5) renderGameStatus('Awesome work! You have reached level 5. Keep going!');
+    } else {
+        turnTimeout = setTimeout(endGame, 10000);
     }
 }
 
+function endGame() {
+    gameStarted = false, gameOver = true;
+    renderGameStatus(`Game over! You reached level ${level}.`);
+}
+
 function renderGameStatus(message) {
-    document.getElementById('game-status').textContent = message;
+    const gameStatusElement = document.getElementById('game-status');
+    gameStatusElement.textContent = message;
+    if(message === '') {
+        gameStatusElement.style.display = 'none'; // Hide the message
+    } else {
+        gameStatusElement.style.display = 'block'; // Show the message
+    }
 }
 
 function renderScore() {
